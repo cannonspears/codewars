20ea6a2afcef55ca760de445b37dca679047e803
Cannon Spears <cannonspears@gmail.com>
2023-08-23 13:30:05 -0500

    Added solution

---
+++ codewars/6kyu/solution.js
@@ -0,0 +1,17 @@
+function solution(str) {
+  var i = 0;
+  var result = new Array();
+  if (str.length % 2 !== 0) {
+    str = str + "_";
+  }
+  while (i < str.length) {
+    result.push(str[i] + str[i + 1]);
+    i += 2;
+  }
+  return result;
+}
+
+// Best Practice Solution
+function solution(s) {
+  return (s + "_").match(/.{2}/g) || [];
+}
