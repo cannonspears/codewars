8f7c26fda7175200259f07dd41dc64d7ba1e32e3
Cannon Spears <cannonspears@gmail.com>
2023-11-13 14:38:32 -0600

    Added simplified code

--- codewars/7kyu/is-this-a-triangle/solution.js
+++ codewars/7kyu/is-this-a-triangle/solution.js
@@ -1,3 +1,10 @@
 function isTriangle(a, b, c) {
   return a + b > c && a + c > b && b + c > a ? true : false;
 }
+
+// Simplified solution
+// No need to explicitly return true/false
+
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
+}
