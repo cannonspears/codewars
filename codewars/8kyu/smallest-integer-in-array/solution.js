94e05e00641bfae2641b09932628713632890066
Cannon Spears <cannonspears@gmail.com>
2023-11-14 21:55:00 -0600

    Added simplified solution

--- codewars/8kyu/smallest-integer-in-array/solution.js
+++ codewars/8kyu/smallest-integer-in-array/solution.js
@@ -2,3 +2,8 @@ function findSmallestInt(args) {
   let sort = args.sort((a, b) => a - b);
   return sort[0];
 }
+
+// Simplified solution: O(n) Time Complexity
+function findSmallestInt(args) {
+  return Math.min(...args);
+}
